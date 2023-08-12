4fd41508ff206f32f1cc37c5e9af6e38c712c153
shivmSingh1 <shivams7380@gmai.com>
2023-08-12 11:44:01 +0530

    replace the given string/char with other string/char

---
+++ javaCode/src/javaBasic/StringReplaceMethod.java
@@ -0,0 +1,10 @@
+package javaBasic;
+
+public class StringReplaceMethod {
+    public static void main(String[] args) {
+
+       final String name= "Shivam";
+        System.out.println(name.replace('S','C'));
+        System.out.println(name.replace("Shi","C"));
+    }
+}
