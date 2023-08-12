b5de13b98d9449c692e3f1c96f44e0689d794086
shivmSingh1 <shivams7380@gmai.com>
2023-08-12 10:25:22 +0530

    uses of Escape sequence tags

---
+++ javaCode/src/javaBasic/EscapeSequenceChar.java
@@ -0,0 +1,10 @@
+package javaBasic;
+
+public class EscapeSequenceChar {
+    public static void main(String[] args) {
+
+        String letter= "Dear Harry, \n\t This Java Course Is Nice.\nthanks";
+        System.out.println(letter);
+
+    }
+}
