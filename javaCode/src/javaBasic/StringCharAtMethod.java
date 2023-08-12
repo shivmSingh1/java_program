d4919f69b368fb63c712b79f4f2a6dab655ca2fe
shivmSingh1 <shivams7380@gmai.com>
2023-08-12 11:50:22 +0530

    find char is present on given index or not

---
+++ javaCode/src/javaBasic/StringCharAtMethod.java
@@ -0,0 +1,9 @@
+package javaBasic;
+
+public class StringCharAtMethod {
+    public static void main(String[] args) {
+
+        final String name="shivam singh";
+        System.out.println("char at index 2 is: "+name.charAt(2));
+    }
+}
