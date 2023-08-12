44d2ba8d024b0280195b0ea4c814ca64f1cadf2d
shivmSingh1 <shivams7380@gmai.com>
2023-08-12 11:37:22 +0530

    make string to substring using subString() method

---
+++ javaCode/src/javaBasic/SubstringMethod.java
@@ -0,0 +1,10 @@
+package javaBasic;
+
+public class SubstringMethod {
+    public static void main(String[] args) {
+         final String sentence = "shivam";
+         String subSentence= sentence.substring(2);
+
+        System.out.println(subSentence);
+    }
+}
