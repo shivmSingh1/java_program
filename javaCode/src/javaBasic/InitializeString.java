312b813ead9bddb086bf83e43e673697a94ad3e3
shivmSingh1 <shivams7380@gmai.com>
2023-08-12 11:07:42 +0530

    string as class and string as data type

---
+++ javaCode/src/javaBasic/InitializeString.java
@@ -0,0 +1,14 @@
+package javaBasic;
+
+public class InitializeString {
+
+    public static void main(String[] args) {
+
+       final String name = new String("shivam"); // string as class
+
+       final String name2= "SHIVAM";
+
+        System.out.println(name);
+        System.out.println(name2);
+    }
+}
