65b1f2889d55b554d1e54eba0db221669dc6e626
shivmSingh1 <shivams7380@gmai.com>
2023-08-13 14:19:49 +0530

    print start pattern

---
+++ javaCode/src/javaBasic/StarPattern.java
@@ -0,0 +1,21 @@
+package javaBasic;
+
+public class StarPattern {
+    public static void main(String[] args) {
+
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=5; j++){
+
+               if(j <= 5-i ){
+
+                   System.out.print("*");
+               } else{
+
+                   System.out.print(" ");
+               }
+            }
+
+            System.out.print("\n");
+        }
+    }
+}
