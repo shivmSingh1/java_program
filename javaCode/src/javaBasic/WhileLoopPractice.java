1cd7d77eafb5e115b5c366e7bdf44b0bd2ef9b93
shivmSingh1 <shivams7380@gmai.com>
2023-08-13 14:18:23 +0530

    print numbers from 100 to 200

---
+++ javaCode/src/javaBasic/WhileLoopPractice.java
@@ -0,0 +1,15 @@
+package javaBasic;
+
+public class WhileLoopPractice {
+    public static void main(String[] args) {
+
+        //print numbers from 100 to 200;
+        int num=100;
+
+        while(num<=200){
+
+            System.out.println(num);
+            num++;
+        }
+    }
+}
